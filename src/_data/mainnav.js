5764977cc8ebb5a5e1485541ac5fa4660b0d495d
Jérôme Coupé <jercoupe@gmail.com>
2025-01-10 07:21:39 +0100

    refactoring WIP

---
+++ src/_data/mainnav.js
@@ -0,0 +1,17 @@
+export default [
+  {
+    navLabel: "Accueil",
+    navUrl: "/",
+    navTrigger: "home",
+  },
+  {
+    navLabel: "Cours",
+    navUrl: "/cours/",
+    navTrigger: "cours",
+  },
+  {
+    navLabel: "Classes",
+    navUrl: "/classes/",
+    navTrigger: "classes",
+  },
+];
