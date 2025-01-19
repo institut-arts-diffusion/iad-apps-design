2dc6e0d0cb7259fa89151f4244fbea1cbb29ff6d
Jérôme Coupé <jercoupe@gmail.com>
2025-01-19 23:02:08 +0100

    sortable datatable, stacks, toasts

--- src/assets/js/modules/mainnav.js
+++ src/assets/js/modules/mainnav.js
@@ -4,9 +4,7 @@ const mobileMenu = document.querySelector(".js-mobilemenu");
 
 function init() {
   menuTriggers.forEach((trigger) => {
-    console.log(trigger);
     trigger.addEventListener("click", (event) => {
-      console.log("clicked");
       event.preventDefault();
       elBody.classList.toggle("has-menu");
       mobileMenu.classList.toggle("is-active");
